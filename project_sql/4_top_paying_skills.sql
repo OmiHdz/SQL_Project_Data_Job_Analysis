bf00963172b150d406fbaf1a9aed834694f58c89
Omar Hdz <omar.hernandez.v95@gmail.com>
2026-02-07 04:10:47 -0600

    Queries completed

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -14,9 +14,11 @@ SELECT
 FROM job_postings_fact
 INNER JOIN skills_job_dim ON skills_job_dim.job_id = job_postings_fact.job_id
 INNER JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
-WHERE job_postings_fact.job_title_short = 'Data Analyst'  --'Data Analyst' / 'Data Scientist'
+WHERE 
+  job_postings_fact.job_title_short = 'Data Analyst'  --'Data Analyst' / 'Data Scientist'
+  AND salary_year_avg IS NOT NULL
 
 GROUP BY skills_dim.skills 
-HAVING AVG(job_postings_fact.salary_year_avg) IS NOT NULL
+-- HAVING AVG(job_postings_fact.salary_year_avg) IS NOT NULL
 ORDER BY avg_salary DESC
 LIMIT 10
